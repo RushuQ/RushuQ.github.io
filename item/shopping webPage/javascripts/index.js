063e0847d5879e31a7039a0bbe16d5f307df4b39
RushuQ <864221359@qq.com>
2017-02-15 10:08:34 +0800

    gzip img&code

--- item/shopping webPage/javascripts/index.js
+++ item/shopping webPage/javascripts/index.js
@@ -1,10 +1,1 @@
-(function(){
-    function autoSlide(){};
-    autoSlide.prototype.init=function(){
-        var  initParam={
-            "slideLi":document.getElementById("")
-        }
-    }
-    var autoSlides=new autoSlide;
-    window.autoSlide=autoSlides;
-})();
+(function(){function autoSlide(){}autoSlide.prototype.init=function(){var initParam={"slideLi":document.getElementById("")}};var autoSlides=new autoSlide;window.autoSlide=autoSlides})();
