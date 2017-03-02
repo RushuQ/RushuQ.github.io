d0a064ce97f64498d52099c6e4e4bb52a47c1351
RushuQ <864221359@qq.com>
2017-03-02 20:37:58 +0800

    route

--- item/webPage/js/index.js
+++ item/webPage/js/index.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         $(this).prev().attr("src",src);
     });
     //右边内容
-    $.getJSON('picData.json', function(date) {
+    $.getJSON('/data/picData', function(date) {
        var community = date.circle;
        var cares = date.myCare;
        var question = date.myWrong;
