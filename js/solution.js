4251d2969ef4cd995a8c1b43a22067122168014d
RushuQ <864221359@qq.com>
2017-03-28 20:52:13 +0800

    left

--- js/solution.js
+++ js/solution.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
       $('.case'+num).show().siblings('.col-right').hide();
     }
     var space = (window.innerWidth - $(".wrap").width())/2;
-    var leftWidth = -(space-space/2) +'px';
+    var leftWidth = (space-space/2) +'px';
     var $navLeft =  $(".col-left");
       $navLeft.css({
-        marginLeft: ''+leftWidth+''
+        left: ''+leftWidth+''
       });
     $(document.body).on("mousewheel",function(){
       document.body.scrollTop >= 200 ? $navLeft.css('top', '25%').addClass('animate') : $navLeft.css('top', '52%');
