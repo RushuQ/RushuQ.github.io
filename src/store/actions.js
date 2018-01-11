9a3de177fc24585df445abbb5c3e4c60fb4ebcb5
rushuQ <864221359@qq.com>
2018-01-12 00:05:52 +0800

    歌词

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,9 +2,21 @@ import * as type from './mutation-type';
 import {playMode} from '@/common/js/config';
 import {shuffe} from "../common/js/util";
 
+function findIndex(list,song) {
+  return list.findIndex(item => {
+    return item.id === song.id;
+  })
+}
 export const selectPlay = function ({commit,state},{list,index}) {
   commit(type.SET_SEQUENCE_LIST, list);
-  commit(type.SET_PLAYLIST, list);
+  if(state.mode === playMode.random){
+    const randomList = shuffe(list);
+    commit(type.SET_PLAYLIST,randomList);
+    index = findIndex(randomList,list[index]);
+    console.log(index);
+  } else {
+    commit(type.SET_PLAYLIST, list);
+  }
   commit(type.SET_CURRENT_INDEX, index);
   commit(type.SET_FULL_SCREEN, true)
   commit(type.SET_PLAYING_STATE,true);
